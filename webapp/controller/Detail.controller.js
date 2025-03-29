b3121d0869e76f5a65c0f7f569508e149b5daf29
esteban2841 <estebanpuentesdev@gmail.com>
2025-03-29 23:20:33 +0000

    prior to try with children table inside smarttable to insert buttons

--- webapp/controller/Detail.controller.js
+++ webapp/controller/Detail.controller.js
@@ -10,7 +10,36 @@ sap.ui.define([
             console.log('entro en detail')
             let oRouter = sap.ui.core.UIComponent.getRouterFor(this);
             oRouter.getRoute("detail").attachPatternMatched(this._onObjectMatched, this);
+
+            //Logic to get the smart table when created and mounted in ui to inject html buttons to the rows 
         },
+        
+        onShowDetails: function(oEvent) {
+            var oBindingContext = oEvent.getSource().getBindingContext();
+            var oSelectedItem = oBindingContext.getObject();
+            
+            // Your dialog opening logic here
+            this.showDetailDialog(oSelectedItem);
+        },
+
+        showDetailDialog: function(oSelectedItem) {
+            if (!this._oDialog) {
+                this._oDialog = sap.ui.xmlfragment(
+                    "your.namespace.fragments.DetailDialog",
+                    this
+                );
+                this.getView().addDependent(this._oDialog);
+            }
+            
+            // Bind the selected item to the dialog
+            this._oDialog.bindElement({
+                path: "/" + oSelectedItem.__metadata.path,
+                model: "yourModelName"
+            });
+            
+            this._oDialog.open();
+        },
+        
    
         _onObjectMatched: function (oEvent) {
             // Obtener el ProductID de la URL y enlazar el contexto
